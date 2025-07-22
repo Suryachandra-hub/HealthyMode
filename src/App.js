faa1a5728920558ea67f34c1365389efbcfff1b8
Suryachandra-hub <nimmagaddasurya4@gmail.com>
2025-07-22 13:35:34 +0530

    Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'Poppins, sans-serif' }}>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
